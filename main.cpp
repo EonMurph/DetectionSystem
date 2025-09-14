9b269812014f95ddd801d19d589d54b17f7edc56
EonMurph <stiger.home@gmail.com>
2025-09-14 19:09:44 +0100

    Refactored search_area code into a class

--- main.cpp
+++ main.cpp
@@ -7,16 +7,33 @@
 using namespace cv;
 using namespace std;
 
-void scan_search_area(int, int, int, int, Mat);
+class Area {
+   public:
+    int x, y, w, h;
+    Rect rect;
+    Area(int x, int y, int w, int h) {
+        this->x = x;
+        this->y = y;
+        this->w = w;
+        this->h = h;
+        Rect r(x, y, w, h);
+        this->rect = r;
+    }
+
+    void scan_area(Mat frame) {
+        for (int i = this->x; i < this->x + this->w; i++) {
+            for (int j = this->y; j < this->y + this->h; j++) {
+                Point center(i, j);
+                circle(frame, center, 0, Scalar(0, 255, 0), -1);
+            }
+        }
+    }
+};
 
 int main() {
     VideoCapture cap;
     Mat frame;
-    int search_x = 600;
-    int search_y = 400;
-    int search_w = 100;
-    int search_h = 100;
-    Rect search_area(search_x, search_y, search_w, search_h);
+    Area search_area(1000, 600, 100, 100);
 
     cap.open(0, CAP_ANY);
     if (!cap.isOpened()) {
@@ -31,8 +48,8 @@ int main() {
             return -1;
         }
 
-        rectangle(frame, search_area, Scalar(0, 0, 255), 3);
-        scan_search_area(search_x, search_y, search_w, search_h, frame);
+        rectangle(frame, search_area.rect, Scalar(0, 0, 255), 3);
+        search_area.scan_area(frame);
         imshow("Camera", frame);
         if (waitKey(1) >= 0) {
             break;
@@ -41,13 +58,4 @@ int main() {
 
     cap.release();
     return 0;
-}
-
-void scan_search_area(int x, int y, int w, int h, Mat frame) {
-    for (int i = x; i < x + w; i++) {
-        for (int j = y; j < y + h; j++) {
-            Point center(i, j);
-            circle(frame, center, 0, Scalar(0, 255, 0), -1);
-        }
-    }
 }
